dff92e107098359660f816afd99b16ea5b5d1beb
Triden Richards <tridenratu@hotmail.com>
2022-10-30 09:18:24 +1300

    testing jump

--- game.js
+++ game.js
@@ -1,18 +1,6 @@
 let character = document.getElementById("character")
 let block = document.getElementById("block")
 
-function jump()
-{
-	if (character.classList != "animate")
-	{
-		character.classList.add("animate")
-	}
-	setTimeout(function()
-	{
-		character.classList.remove("animate")
-	}, 500)
-}
-
 var checkDead = setInterval(function()
 {
 	var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
