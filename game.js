edfac9f8ffb9af91c8e27285d70b5c2bf43bb48b
Triden Richards <tridenratu@hotmail.com>
2022-10-30 09:03:36 +1300

    game update

---
+++ game.js
@@ -0,0 +1,27 @@
+let character = document.getElementById("character")
+let block = document.getElementById("block")
+
+function jump()
+{
+	if (character.classLIst != "animate")
+	{
+		character.classList.add("animate")
+	}
+	setTimeout(function()
+	{
+		character.classList.remove("animate")
+	}, 500)
+}
+
+var checkDead = setInterval(function()
+{
+	var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+	var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+	if(blockLeft<20 && blockLeft>0 && characterTop>=130)
+	{	
+		block.style.animation = "none"
+		block.style.display = "none"
+		alert("Game Over")
+	}
+}, 10)
+
