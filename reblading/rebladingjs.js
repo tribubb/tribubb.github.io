d6bcae16be2d113e18654e92d2168f6ca514ca54
Triden Richards <tridenratu@hotmail.com>
2022-11-04 17:08:25 +1300

    fjfjkjhj

--- reblading/rebladingjs.js
+++ reblading/rebladingjs.js
@@ -2,3 +2,19 @@ function home()
 {
 	document.getElementById("subject").innerHTML = "testing123";
 }
+
+function aboutus()
+{
+	document.getElementById("subject").innerHTML = "About us:
+			Landline: 043454001
+			Mobile: 021444888
+			Email: tanneryreblading@test.com";
+}
+
+function contactus()
+{
+	document.getElementById("subject").innerHTML = "Contact us:
+			Landline: 043454002
+			Mobile: 021444888
+			Email: tanneryreblading@test.com";
+}
