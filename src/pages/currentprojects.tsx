62e64b4c37039ab8717659f0bfb808baee8d3280
Triden Richards <tridenratu@hotmail.com>
2023-03-20 11:05:12 +1300

    testing game download

--- src/pages/currentprojects.tsx
+++ src/pages/currentprojects.tsx
@@ -61,59 +61,15 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            href="https://www.dropbox.com/s/u11nzj7sj1a295k/BubbRTSv.023.rar?dl=0"
             className={styles.card}
             target="_blank"
-            rel="noopener noreferrer"
           >
             <h2 className={inter.className}>
-              Current Projects <span>-&gt;</span>
+              BubbRTS <span>-&gt;</span>
             </h2>
             <p className={inter.className}>
-              View Projects currently under development.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
+             Newest version released <code>10/03/2023 07:29am</code>
             </p>
           </a>
         </div>
