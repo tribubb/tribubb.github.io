7f82bcfd022c555bcd99e590ed7c4c224afb1fdd
Triden Richards <tridenratu@hotmail.com>
2023-04-10 17:48:17 +1200

    password page border

--- src/pages/JobApplications/CVCLJA928354.tsx
+++ src/pages/JobApplications/CVCLJA928354.tsx
@@ -17,9 +17,11 @@ export default function Home() {
       <main className={styles.main}>
 
         <div className={styles.center}>
-	<p>You have reached the Job Application Portal. This has been started on the 4th of April to monitor new application statuses.</p>
-	<p>I will be providing the CV used, my Cover Letter used and the status of the Job Application</p>
-        <p>In addition I will gauge the suitability for the role with my skillset, and possibly location, green and potentially any red flags</p>
+            <div className={styles.basicBorder}>
+	            <p>You have reached the Job Application Portal. This has been started on the 4th of April to monitor new application statuses.</p>
+	            <p>I will be providing the CV used, my Cover Letter used and the status of the Job Application</p>
+                <p>In addition I will gauge the suitability for the role with my skillset, and possibly location, green and potentially any red flags</p>
+            </div>
         </div>
 
         <div className={styles.grid}>
