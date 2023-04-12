cb80e4f7097b6210069f05578b5473eeba3cf4cf
Triden Richards <tridenratu@hotmail.com>
2023-04-13 00:46:06 +1200

    simple.js fix attempt 1

--- src/pages/PHPRevival/simple.js
+++ src/pages/PHPRevival/simple.js
@@ -1,11 +1,10 @@
 // file simpleajax.js
 var xhr = createRequest();
-function getData(dataSource, divID, aYear, aPop, aMil, aEco, aTech, aCivpoints, aCivpointpop, aCivpointmil, aCivpointeco,
-		   aCivpointtech, aTechstate, aEvent);  {
-    if(xhr) {
-	    
-	    getData('focus.php','targetDiv', year.value, pop.value, mil.value, eco.value, tech.value, civpoints.value, civpointpop.value, 
-		    civpointmil.value, civpointeco.value, civpointtech.value, techstate.value, event.value) ,1000);
+function getData(dataSource, divID, aYear, aPop, aMil, aEco, aTech, aCivpoints, aCivpointpop, aCivpointmil, aCivpointeco, aCivpointtech, aTechstate, aEvent)
+{
+	if (xhr)
+	{
+		getData(('focus.php', 'targetDiv', year.value, pop.value, mil.value, eco.value, tech.value, civpoints.value, civpointpop.value, civpointmil.value, civpointeco.value, civpointtech.value, techstate.value), 1000);
 	    
 	    var place = document.getElementById(divID);
 	    var url = dataSource+"?year="+aYear+"?pop="+aPop+"?mil="+aMil+"?eco="+aEco
@@ -13,10 +12,12 @@ function getData(dataSource, divID, aYear, aPop, aMil, aEco, aTech, aCivpoints, 
 	    +"?civpointmil="+aCivpointmil+"?civpointeco="+aCivpointeco
 	    +"?civpointtech="+aCivpointtech+"?techstate="+aTechstate+"?event="+aEvent;
 	    xhr.open("GET", url, true);
-	    xhr.onreadystatechange = function() {
+		xhr.onreadystatechange = function ()
+		{
 		    alert(xhr.readyState);
-			if (xhr.readyState == 4 && xhr.status == 200) {
-			place.innerHTML = xhr.responseText;
+			if (xhr.readyState == 4 && xhr.status == 200)
+			{
+				place.innerHTML = xhr.responseText;
 		    } // end if
 	    } // end anonymous call-back function
 	    xhr.send(null);
