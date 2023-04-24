115ec6116428144b41bf5df211dc472cb72b902c
Triden Richards <tridenratu@hotmail.com>
2023-04-24 20:28:11 +1200

    adding class to p

--- src/pages/JobApplications/CVCLJA928354.tsx
+++ src/pages/JobApplications/CVCLJA928354.tsx
@@ -17,9 +17,9 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.center}>
             <div className={styles.basicBorder}>
-                <p>You have reached the Job Application Portal. This has been started on the 4th of April to monitor new application statuses.</p><br/>
-                <p>I will be providing the CV used, my Cover Letter used and the status of the Job Application</p><br/>
-                <p>In addition I will gauge the suitability for the role with my skillset, and possibly location, green and potentially any red flags</p>
+                <p className={inter.className}>You have reached the Job Application Portal. This has been started on the 4th of April to monitor new application statuses.</p><br/>
+                <p className={inter.className}>I will be providing the CV used, my Cover Letter used and the status of the Job Application</p><br/>
+                <p className={inter.className}>In addition I will gauge the suitability for the role with my skillset, and possibly location, green and potentially any red flags</p>
             </div>
         </div>
 
