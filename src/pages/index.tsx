47ca703b0bf92bb9fa8a5fc8e324e1944e07f3d1
Triden Richards <tridenratu@hotmail.com>
2023-05-03 11:56:12 +1200

    attemping to reinclude video in main

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,15 +39,14 @@ export default function Home() {
               View other projects.
             </p>
           </a>
-        </div>             
-      </main>
-
-      <div className={styles.animatedMain}>
+        </div>  
+        <div className={styles.animatedMain}>
           <video autoplay muted loop>
-              <source src="@/videos/BubbPlanet.mp4" type="video/mp4">
-                  Your browser does not support the video tag.
+            <source src="@/videos/BubbPlanet.mp4" type="video/mp4">
+              Your browser does not support the video tag.
           </video>
-      </div>
+        </div>
+      </main>
     </>
   )
 }
