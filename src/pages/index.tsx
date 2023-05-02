796beadd3e94b12dfff4c11b2e959c4541ac5fb8
Triden Richards <tridenratu@hotmail.com>
2023-05-03 10:11:41 +1200

    attempting video background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,45 +13,41 @@ export default function Home() {
         <meta name="description" content="Welcome to Triden's official project site" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head>  
       <main className={styles.main}>
-          </main>
-          <div className={styles.animatedMain}>
-              <div className={styles.spin}>
-                  <a href="BubbRTS/bubbRTSLanding">
-                      <code className={styles.code}>BubbRTS v.025 available here</code>
-                  </a>
-              </div>
+        <div className={styles.spin}>
+          <a href="BubbRTS/bubbRTSLanding">
+          <code className={styles.code}>BubbRTS v.025 available here</code>
+          </a>
+        </div>
 
-              <div className={styles.center}>
-              </div>
+        <div className={styles.animatedMain}>
+          <video autoplay muted loop>
+            <source src="videos/BubbPlanet.mp4" type="video/mp4">
+            Your browser does not support the video tag.
+          </video>
+        </div>
 
-              <div className={styles.grid}>
-                  <a
-                      href="unityprojects"
-                      className={styles.card}
-                  >
-                      <h2 className={inter.className}>
-                          Unity Projects<span>-&gt;</span>
-                      </h2>
-                      <p className={inter.className}>
-                          View projects currently under development.
-                      </p>
-                  </a>
+        <div className={styles.grid}>
+          <a href="unityprojects" className={styles.card}>
+            <h2 className={inter.className}>
+              Unity Projects<span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+               View projects currently under development.
+            </p>
+          </a>
 
-                  <a
-                      href="otherprojects"
-                      className={styles.card}
-                  >
-                      <h2 className={inter.className}>
-                          Other Projects<span>-&gt;</span>
-                      </h2>
-                      <p className={inter.className}>
-                          View other projects.
-                      </p>
-                  </a>
-              </div>
-          </div>
+          <a href="otherprojects" className={styles.card}>
+            <h2 className={inter.className}>
+              Other Projects<span>-&gt;</span>
+            </h2>
+            <p className={inter.className}>
+              View other projects.
+            </p>
+          </a>
+        </div>             
+      </main>
     </>
   )
 }
