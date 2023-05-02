64b0dcac106999ea5442c8c667d1f6d7d792f2f8
Triden Richards <tridenratu@hotmail.com>
2023-05-03 08:43:00 +1200

    animated colour component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,41 +15,41 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.spin}>
-          <a
-            href="BubbRTS/bubbRTSLanding"
-          >
-            <code className={styles.code}>BubbRTS v.025 available here</code>
-          </a>
-        </div>
+        <div className={styles.animatedMain}>
+           <div className={styles.spin}>
+                      <a href="BubbRTS/bubbRTSLanding">
+                          <code className={styles.code}>BubbRTS v.025 available here</code>
+                      </a>
+                  </div>
 
-        <div className={styles.center}>
-        </div>
+                  <div className={styles.center}>
+                  </div>
 
-        <div className={styles.grid}>
-          <a
-            href="unityprojects"
-            className={styles.card}
-          >
-            <h2 className={inter.className}>
-              Unity Projects<span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              View projects currently under development.
-            </p>
-          </a>
+                  <div className={styles.grid}>
+                      <a
+                          href="unityprojects"
+                          className={styles.card}
+                      >
+                          <h2 className={inter.className}>
+                              Unity Projects<span>-&gt;</span>
+                          </h2>
+                          <p className={inter.className}>
+                              View projects currently under development.
+                          </p>
+                      </a>
 
-	  <a
-            href="otherprojects"
-            className={styles.card}
-          >
-            <h2 className={inter.className}>
-               Other Projects<span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              View other projects.
-            </p>
-          </a>
+                      <a
+                          href="otherprojects"
+                          className={styles.card}
+                      >
+                          <h2 className={inter.className}>
+                              Other Projects<span>-&gt;</span>
+                          </h2>
+                          <p className={inter.className}>
+                              View other projects.
+                          </p>
+                      </a>
+                  </div>
         </div>
       </main>
     </>
