c7f78b29ddea227cf839f588af9b9e624e0c54ea
Triden Richards <tridenratu@hotmail.com>
2023-05-04 10:29:39 +1200

    Revert "testing yarn addon"
    This reverts commit 848b4d8f58cc0ad82dacbce1dcb64c3e40908c95.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
-const withVideos = require('next-videos');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-};
+  reactStrictMode: true,
+}
 
-module.exports = withVideos(nextConfig);
+module.exports = {
+    nextConfig,
+    withVideos(),
+}
+
