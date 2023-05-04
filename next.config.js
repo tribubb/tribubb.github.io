23a8d87f6c260eaba192ffc658bd05e6bbd47eb7
Triden Richards <tridenratu@hotmail.com>
2023-05-04 12:37:16 +1200

    fixed package.json

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
+const withVideos = require('next-videos');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = withVideos(nextConfig);
