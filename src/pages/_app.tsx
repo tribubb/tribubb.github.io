e1b5248e9e6ad02e0d443dbadf7815ac0337cfe7
Triden Richards <tridenratu@hotmail.com>
2023-06-19 10:06:02 +1200

    Attempting seamless change to prevent reloading background video.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,28 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+
+    useEffect(() => {
+        const handleRouteChangeStart = (url: string) => {
+            console.log('Route change started:', url)
+        }
+
+        const handleRouteChangeComplete = (url: string) => {
+            console.log('Route change completed:', url)
+        }
+
+        router.events.on('routeChangeStart', handleRouteChangeStart)
+        router.events.on('routeChangeComplete', handleRouteChangeComplete)
+
+        return () => {
+            router.events.off('routeChangeStart', handleRouteChangeStart)
+            router.events.off('routeChangeComplete', handleRouteChangeComplete)
+        }
+    }, [])
+
+    return <Component {...pageProps} />
 }
