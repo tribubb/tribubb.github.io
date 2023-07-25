805fb170160e2d70b022e003014495918f770eee
Triden Richards <tridenratu@hotmail.com>
2023-07-25 21:00:14 +1200

    Added many more links

--- src/pages/otherprojects.tsx
+++ src/pages/otherprojects.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -70,7 +71,7 @@ export default function Home() {
                       </p>
                   </a>
 
-                  <a
+                  <Link
                       href="/"
                       className={styles.card}
                   >
@@ -80,7 +81,7 @@ export default function Home() {
                       <p className={inter.className}>
                           Return to main page
                       </p>
-                  </a>
+                  </Link>
               </div>
           </main>
     </>
