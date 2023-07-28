032008b3012a751e31832e5395b1a8b5d9f15107
Triden Richards <tridenratu@hotmail.com>
2023-07-28 19:30:00 +1200

    Adjusted Living Price added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,13 +31,19 @@ export default function Home() {
                             <h2 className={inter.className}>
                                 Unity Projects<span>-&gt;</span>
                             </h2>
-                            <p className={inter.className}>View projects currently under development.</p>
+                            <p className={inter.className}>View Unity projects currently under development.</p>
                     </Link>
                     <Link href="/otherprojects" className={styles.card}>
                         <h2 className={inter.className}>
                             Other Projects<span>-&gt;</span>
                         </h2>
-                        <p className={inter.className}>View other projects.</p>
+                        <p className={inter.className}>View other external projects.</p>
+                    </Link>
+                    <Link href="/localprojects" className={styles.card}>
+                        <h2 className={inter.className}>
+                            Local Projects<span>-&gt;</span>
+                        </h2>
+                        <p className={inter.className}>View a project within this domain.</p>
                     </Link>
                 </div>
             </main>
